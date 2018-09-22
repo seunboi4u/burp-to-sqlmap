0c98623e0ffac83eb9f1b9482026d29eeeebfd4f
Milad Khoshdel <miladkhoshdel@gmail.com>
2018-09-22 15:06:35 +0330

    Proxy Feature Added.
    - Proxy Feature Added.
    - Bugs fixed in Result Section.

--- burp-to-sqlmap.py
+++ burp-to-sqlmap.py
@@ -9,6 +9,7 @@ except ImportError:
     print "[!] wrong installation detected (missing modules)."
     exit()
 
+
 def banner():
     print " "
     print " #######################################################################"
@@ -30,13 +31,15 @@ def banner():
     print " #######################################################################"
     print " "
 
+
 def usage():
     print" "
     print"  Usage: ./burp-to-sqlmap.py [options]"
     print"  Options: -f, --file               <BurpSuit State File>"
     print"  Options: -o, --outputdirectory    <Output Directory>"
     print"  Options: -s, --sqlmappath         <SQLMap Path>"
-    print"  Example: python burp-to-sqlmap.py -f [BURP-STATE-FILE] -o [OUTPUT-DIRECTORY] -s [SQLMap-Path]"
+    print"  Options: -p, --proxy              <Use Proxy>"
+    print"  Example: python burp-to-sqlmap.py -f [BURP-STATE-FILE] -o [OUTPUT-DIRECTORY] -s [SQLMap-Path] -p [Proxy]"
     print" "
 
 
@@ -44,14 +47,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file")
 parser.add_argument("-o", "--outputdirectory")
 parser.add_argument("-s", "--sqlmappath")
+parser.add_argument("-p", "--proxy")
 args = parser.parse_args()
-
 
 if not args.file or not args.outputdirectory or not args.sqlmappath:
     banner()
     usage()
     sys.exit(0)
 
+if args.proxy:
+    proxyvalue = "--proxy " + args.proxy
+else:
+    proxyvalue = ""
+
+vulnerablefiles = []
 banner()
 filename = args.file
 directory = args.outputdirectory
@@ -64,7 +73,7 @@ print " [+] Exporting Packets ..."
 with open(filename, 'r') as f:
     soup = BeautifulSoup(f.read(), "html.parser")
     for i in soup.find_all("request"):
-        packetnumber = packetnumber+1
+        packetnumber = packetnumber + 1
         print "   [-] Packet " + str(packetnumber) + " Exported."
         outfile = open(os.path.join(args.outputdirectory, str(packetnumber) + ".txt"), "w")
         outfile.write(i.text.strip())
@@ -75,10 +84,24 @@ with open(filename, 'r') as f:
 print " [+] Testing SQL Injection on packets ...  (Based on your network connection Test can take up to 5 minutes.)"
 for file in os.listdir(directory):
     print "   [-] Performing SQL Injection on packet number " + file[:-4] + ". Please Wait ..."
-    os.system("python " + sqlmappath + "\sqlmap.py -r " + os.path.dirname(os.path.realpath(__file__))+ "\\" + directory + "\\" + file + " --batch > " + os.path.dirname(os.path.realpath(__file__)) + "\\" + directory + "\\testresult" + file)
-    if 'is vulnerable' in open(directory + "\\testresult" + file).read() or "Payload:" in open(directory + "\\testresult" + file).read():
+    os.system("python " + sqlmappath + "\sqlmap.py -r " + os.path.dirname(os.path.realpath(
+        __file__)) + "\\" + directory + "\\" + file + " --batch " + proxyvalue + " > " + os.path.dirname(
+        os.path.realpath(__file__)) + "\\" + directory + "\\testresult" + file)
+    if 'is vulnerable' in open(directory + "\\testresult" + file).read() or "Payload:" in open(
+            directory + "\\testresult" + file).read():
         print "    - URL is Vulnerable."
+        vulnerablefiles.append(file)
     else:
         print "    - URL is not Vulnerable."
     print "    - Output saved in " + directory + "\\testresult" + file
-    print " "
+print " "
+print "--------------"
+print "Test Done."
+print "Result:"
+if not vulnerablefiles:
+    print "No vulnerabilities found on your target."
+else:
+    for items in vulnerablefiles:
+        print "Packet " + items[:-4] + " is vulnerable to SQL Injection. for more information please see " + items
+print "--------------"
+print " "
